e35359e99d56ca6d6d78be5e368d50a789fb9506
bspatafora <benjamin.spatafora@gmail.com>
2014-05-05 17:11:25 -0500

    Add tests

--- spec/mastermind_sinatra_spec.rb
+++ spec/mastermind_sinatra_spec.rb
@@ -10,8 +10,25 @@ end
 RSpec.configure { |c| c.include RSpecMixin }
 
 describe 'mastermind_sinatra' do
-  it "allows accessing the main page" do
+  before do
+    new_board = Mastermind::Board.new
+    @new_board_structure = Mastermind::JSONInterface.generate_structure_from(new_board)
+    @JSON_board = JSON.generate(@new_board_structure)
+  end
+
+  it "gets a page with a board" do
     get '/'
+    expect(last_response.body).to include("<table id='board'>")
+  end
+
+  it "gets a JSON new board" do
+    get '/new_board'
+    expect(JSON.parse(last_response.body, :symbolize_names=>true)).to eql(@new_board_structure)
+  end
+
+  it "accepts a JSON board post" do
+    body = @JSON_board
+    post '/send', body, {'Content-Type' => 'application/json'}
     expect(last_response).to be_ok
   end
 end
